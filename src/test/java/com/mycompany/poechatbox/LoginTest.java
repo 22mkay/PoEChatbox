0ea237af5b36386f6be6fc75351bef8366d638d2
Student <Student@LabVM2049939.4wf4kdv4hhvutoyhxx405kq0mg.jnbx.internal.cloudapp.net>
2026-09-18 18:08:05 +0200

    I created the logintest.java file, i added few things i am going to add the JUnit now

--- src/test/java/com/mycompany/poechatbox/LoginTest.java
+++ src/test/java/com/mycompany/poechatbox/LoginTest.java
@@ -4,10 +4,29 @@
  */
 package com.mycompany.poechatbox;
 
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 /**
  *
  * @author Student
  */
+
+
 public class LoginTest {
+    private Login login;
+
+    @Before // Notice this is @Before in JUnit 4, not @BeforeEach
+    public void setUp() {
+        login = new Login();
+        login.registerUser("kyl_1", "Ch&&sec@ke99!", "Kyle", "Doe");
+    }
+
+    @Test
+    public void testCheckUserNameCorrect() {
+        assertTrue(login.checkUserName("kyl_1"));
+    }
+    
     
 }
